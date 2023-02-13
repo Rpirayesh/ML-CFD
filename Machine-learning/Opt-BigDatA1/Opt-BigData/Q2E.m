1cb03c935b07e9d1e07460c7e83e1d9e4dba7e7e
reza <rpirayeshs@gmail.com>
2023-02-13 14:20:43 -0700

    3 ML

--- Machine-learning/Opt-BigDatA1/Opt-BigData/Q2E.m
+++
@@ -1,9 +0,0 @@
-function y=Q2E(Input)
-Rq=(Input(1,4)^2-Input(1,1:3)*Input(1,1:3)')*eye(3)+2*Input(1,1:3)'*Input(1,1:3)-2*Input(1,4)*skew(Input(1,1:3)');
-if abs(Rq(1,3))>1
-    Rq(1,3)=sign(Rq(1,3));
-end
-tetaE=asin(-Rq(1,3));
-phiE=atan2(sign(cos(tetaE))*Rq(1,2),sign(cos(tetaE))*Rq(1,1));
-sayE=atan2(sign(cos(tetaE))*Rq(2,3),sign(cos(tetaE))*Rq(3,3));
-y=[phiE,tetaE,sayE];
--- Machine-learning/test.py
+++
@@ -1,9 +0,0 @@
-Git intructions
-
-git clone https://github.com/Rpirayesh/Deep-learning.git
-
-git add test.py
-
-git commit -m "Test to commit a file"
-
-git push
